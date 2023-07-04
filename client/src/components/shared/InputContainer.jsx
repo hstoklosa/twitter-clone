e81d1a66fb682a8b912b2cfeda8eb4b572a6c4c3
imexotic <hubert.stoklosa23@gmail.com>
2023-07-05 00:49:11 +0100

    create input components for reusability

---
+++ client/src/components/shared/InputContainer.jsx
@@ -0,0 +1,39 @@
+import { IconContext } from "react-icons";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useEffect, useState } from "react";
+
+const InputContainer = ({ type, id, name, value, onChange, onBlur, onFocus, label, disabled, error, setError }) => {
+    const [isVisible, setVisible] = useState(false);
+
+    const toggle = () => setVisible(!isVisible);
+
+    return (
+        <div className="input-container">
+            <input
+                type={isVisible ? "text" : type}
+                id={id}
+                name={name}
+                className={`${value ? "not-empty" : ""} ${error && "error"}`}
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                disabled={disabled}
+            />
+
+            <label htmlFor={name}>{label || name.charAt(0).toUpperCase() + name.slice(1)}</label>
+
+            {error && <p className="error-message">{error}</p>}
+
+            {type === "password" && (
+                <button type="button" className="btn-show-password" onClick={toggle}>
+                    <IconContext.Provider value={{ className: "btn-icon" }}>
+                        {isVisible ? <AiOutlineEyeInvisible size="22" /> : <AiOutlineEye size="22" />}
+                    </IconContext.Provider>
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default InputContainer;
