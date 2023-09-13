690e79cd786b8e6f71b7ac6fa6438416bb3882a2
imexotic <hubert.stoklosa23@gmail.com>
2023-09-13 20:17:50 +0100

    fix useOutsideClick hook

--- client/src/hooks/useOutsideClick.jsx
+++ client/src/hooks/useOutsideClick.jsx
@@ -6,14 +6,12 @@ const useOutsideClick = (callback) => {
     const handleClick = (e) => {
         if (ref.current && !ref.current.contains(e.target)) {
             callback();
-            document.body.style.overflow = "unset";
         }
     };
 
     const handleEscape = (e) => {
         if (e.key === "Escape") {
             callback();
-            document.body.style.overflow = "unset";
         }
     };
 
@@ -25,7 +23,7 @@ const useOutsideClick = (callback) => {
             document.removeEventListener("mousedown", handleClick);
             document.removeEventListener("keydown", handleEscape);
         };
-    }, [ref, callback]);
+    }, [ref]);
 
     return ref;
 };
