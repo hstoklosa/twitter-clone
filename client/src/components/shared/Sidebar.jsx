bb16a7c93e9ad92bcf89565d0a75f8f8b1c433eb
imexotic <hubert.stoklosa23@gmail.com>
2023-09-16 23:27:46 +0100

    change to rtk query in sidebar

--- client/src/components/shared/Sidebar.jsx
+++ client/src/components/shared/Sidebar.jsx
@@ -1,11 +1,8 @@
 import logo from "../../assets/logo-white.png";
 
-import { useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 
 import { IconContext } from "react-icons";
-import { FaFeatherAlt } from "react-icons/fa";
 import {
     BiSearch,
     BiHomeCircle,
@@ -20,167 +17,207 @@ import {
     BiSolidUser,
     BiCog,
 } from "react-icons/bi";
+import { FaFeatherAlt } from "react-icons/fa";
 import { IoEllipsisHorizontal } from "react-icons/io5";
 import { PiDotsThreeCircle } from "react-icons/pi";
 
-import { FloatOptions } from "../index";
-import { signOut } from "../../slices/authSlice";
+import { useLazySignOutQuery } from "../../store/api/authApi";
+import { useGetUserInfoQuery } from "../../store/api/userApi";
 
-const Sidebar = ({ minimal }) => {
+const Sidebar = ({ minimal, currentUsername }) => {
+    const { pathname } = useLocation();
     const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.auth.user);
 
-    const [accountFloat, setAccountFloat] = useState(false);
+    const { username, displayName, profileImageURL } = useGetUserInfoQuery(currentUsername, {
+        selectFromResult: ({ data }) => ({
+            username: data?.username,
+            displayName: data?.displayName,
+            profileImageURL: data?.profileImageURL,
+        }),
+        skip: !currentUsername,
+    });
 
-    const openAccountFloat = () => setAccountFloat(true);
-    const closeAccountFloat = () => setAccountFloat(false);
+    const [signOut] = useLazySignOutQuery();
 
     const handleSignOut = async () => {
-        const result = await dispatch(signOut());
+        const result = await signOut();
 
-        if (result.error) {
-            return;
+        if (!result.isError) {
+            navigate("/");
         }
-
-        // event handler navigation
-        closeAccountFloat();
-        navigate("/");
-        return;
     };
 
     return (
-        <section className="column" id="navbar">
+        <section
+            className="column"
+            id="navbar"
+        >
             <div className="sticky-wrapper">
-                <NavLink to={`/`} className="logo-container">
-                    <img src={logo} alt="Logo" />
+                <NavLink
+                    to={`/`}
+                    className="logo-container"
+                >
+                    <img
+                        src={logo}
+                        alt="Logo"
+                    />
                 </NavLink>
 
                 {minimal && (
-                    <nav className="navbar">
-                        <NavLink to={`/explore`} className="navbar-link current" style={{ cursor: "not-allowed" }}>
-                            <IconContext.Provider value={{ className: "navbar_icon explore" }}>
+                    <IconContext.Provider value={{ className: "navbar_icon explore" }}>
+                        <nav className="navbar">
+                            <NavLink
+                                to={`/explore`}
+                                className="navbar-link current"
+                                style={{ cursor: "not-allowed" }}
+                            >
                                 <BiSearch size="25" />
-                            </IconContext.Provider>
-                            <span className="text">Explore</span>
-                        </NavLink>
-                        <NavLink to={`/settings`} className="navbar-link">
-                            <IconContext.Provider value={{ className: "navbar_icon explore" }}>
+                                <span className="text">Explore</span>
+                            </NavLink>
+
+                            <NavLink
+                                to={`/settings`}
+                                className="navbar-link"
+                            >
                                 <BiCog size="25" />
-                            </IconContext.Provider>
-                            <span className="text">Settings</span>
-                        </NavLink>
-                    </nav>
+                                <span className="text">Settings</span>
+                            </NavLink>
+                        </nav>
+                    </IconContext.Provider>
                 )}
 
                 {!minimal && (
-                    <nav className="navbar">
-                        <NavLink
-                            to={`/home`}
-                            className="navbar-link"
-                            children={({ isActive }) => (
-                                <>
-                                    <IconContext.Provider value={{ className: "navbar_icon explore" }}>
-                                        {isActive ? <BiSolidHomeCircle size="25" /> : <BiHomeCircle size="25" />}
-                                    </IconContext.Provider>
-                                    <span className="text">Home</span>
-                                </>
-                            )}
-                        />
-                        <NavLink
-                            to={`/explore`}
-                            className="navbar-link"
-                            children={({ isActive }) => (
-                                <>
-                                    <IconContext.Provider value={{ className: "navbar_icon explore" }}>
+                    <IconContext.Provider value={{ className: "navbar_icon" }}>
+                        <nav className="navbar">
+                            <NavLink
+                                to={`/home`}
+                                className="navbar-link"
+                                children={({ isActive }) => (
+                                    <>
+                                        {isActive ? (
+                                            <BiSolidHomeCircle size="25" />
+                                        ) : (
+                                            <BiHomeCircle size="25" />
+                                        )}
+                                        <span className="text">Home</span>
+                                    </>
+                                )}
+                                state={{ previousPath: pathname }}
+                            />
+
+                            <NavLink
+                                to={`/explore`}
+                                className="navbar-link"
+                                children={({ isActive }) => (
+                                    <>
                                         <BiSearch size="25" />
-                                    </IconContext.Provider>
-                                    <span className="text">Explore</span>
-                                </>
-                            )}
-                        />
-                        <NavLink
-                            to={`/notifications`}
-                            className="navbar-link"
-                            children={({ isActive }) => (
-                                <>
-                                    <IconContext.Provider value={{ className: "navbar_icon settings" }}>
-                                        {isActive ? <BiSolidBell size="25" /> : <BiBell size="25" />}
-                                    </IconContext.Provider>
-                                    <span className="text">Notifications</span>
-                                </>
-                            )}
-                        />
-                        <NavLink
-                            to={`/messages`}
-                            className="navbar-link"
-                            children={({ isActive }) => (
-                                <>
-                                    <IconContext.Provider value={{ className: "navbar_icon settings" }}>
-                                        {isActive ? <BiSolidEnvelope size="25" /> : <BiEnvelope size="25" />}
-                                    </IconContext.Provider>
-                                    <span className="text">Messages</span>
-                                </>
-                            )}
-                        />
-                        <NavLink
-                            to={`/bookmarks`}
-                            className="navbar-link"
-                            children={({ isActive }) => (
-                                <>
-                                    <IconContext.Provider value={{ className: "navbar_icon settings" }}>
-                                        {isActive ? <BiSolidBookmark size="25" /> : <BiBookmark size="25" />}
-                                    </IconContext.Provider>
-                                    <span className="text">Bookmarks</span>
-                                </>
-                            )}
-                        />
-                        <NavLink
-                            to={`/${user.username}`}
-                            className="navbar-link"
-                            children={({ isActive }) => (
-                                <>
-                                    <IconContext.Provider value={{ className: "navbar_icon settings" }}>
-                                        {isActive ? <BiSolidUser size="25" /> : <BiUser size="25" />}
-                                    </IconContext.Provider>
-                                    <span className="text">Profile</span>
-                                </>
-                            )}
-                        />
+                                        <span className="text">Explore</span>
+                                    </>
+                                )}
+                                state={{ previousPath: pathname }}
+                            />
 
-                        <button type="button" className="navbar-link">
-                            <IconContext.Provider value={{ className: "navbar_icon settings" }}>
-                                <PiDotsThreeCircle size="25" />
-                            </IconContext.Provider>
-                            <span className="text">More</span>
-                        </button>
+                            <NavLink
+                                to={`/notifications`}
+                                className="navbar-link"
+                                children={({ isActive }) => (
+                                    <>
+                                        {isActive ? (
+                                            <BiSolidBell size="25" />
+                                        ) : (
+                                            <BiBell size="25" />
+                                        )}
+                                        <span className="text">Notifications</span>
+                                    </>
+                                )}
+                                state={{ previousPath: pathname }}
+                            />
 
-                        <button type="button" className="blue-btn navbar-btn">
-                            <span className="text">Tweet</span>
-                            <IconContext.Provider value={{ className: "navbar-btn_icon" }}>
-                                <FaFeatherAlt size="15" />
-                            </IconContext.Provider>
-                        </button>
-                    </nav>
+                            <NavLink
+                                to={`/messages`}
+                                className="navbar-link"
+                                children={({ isActive }) => (
+                                    <>
+                                        {isActive ? (
+                                            <BiSolidEnvelope size="25" />
+                                        ) : (
+                                            <BiEnvelope size="25" />
+                                        )}
+                                        <span className="text">Messages</span>
+                                    </>
+                                )}
+                                state={{ previousPath: pathname }}
+                            />
+
+                            <NavLink
+                                to={`/bookmarks`}
+                                className="navbar-link"
+                                children={({ isActive }) => (
+                                    <>
+                                        {isActive ? (
+                                            <BiSolidBookmark size="25" />
+                                        ) : (
+                                            <BiBookmark size="25" />
+                                        )}
+                                        <span className="text">Bookmarks</span>
+                                    </>
+                                )}
+                                state={{ previousPath: pathname }}
+                            />
+
+                            <NavLink
+                                to={`/${username}`}
+                                className="navbar-link"
+                                children={({ isActive }) => (
+                                    <>
+                                        {isActive ? (
+                                            <BiSolidUser size="25" />
+                                        ) : (
+                                            <BiUser size="25" />
+                                        )}
+                                        <span className="text">Profile</span>
+                                    </>
+                                )}
+                                state={{ previousPath: pathname }}
+                            />
+
+                            <button
+                                type="button"
+                                className="navbar-link"
+                            >
+                                <IconContext.Provider value={{ className: "navbar_icon settings" }}>
+                                    <PiDotsThreeCircle size="25" />
+                                </IconContext.Provider>
+                                <span className="text">More</span>
+                            </button>
+
+                            <button
+                                type="button"
+                                className="blue-btn navbar-btn"
+                            >
+                                <span className="text">Tweet</span>
+                                <IconContext.Provider value={{ className: "navbar-btn_icon" }}>
+                                    <FaFeatherAlt size="15" />
+                                </IconContext.Provider>
+                            </button>
+                        </nav>
+                    </IconContext.Provider>
                 )}
 
                 {!minimal && (
                     <>
-                        {accountFloat && (
-                            <FloatOptions isOpen={accountFloat} onClose={closeAccountFloat} className="account-settings">
-                                <button type="button" className="more-btn" onClick={handleSignOut}>
-                                    Log out @{user.username}
-                                </button>
-                            </FloatOptions>
-                        )}
-                        <button className="navbar-account" onClick={openAccountFloat}>
+                        <button className="navbar-account">
                             <div className="pfp-container">
-                                <img src={user?.profileImageURL} className="pfp" alt="User PFP" />
+                                <img
+                                    src={profileImageURL}
+                                    className="pfp"
+                                    alt="User PFP"
+                                />
                             </div>
 
                             <div className="navbar-account_names">
-                                <p className="display_name">{user?.displayName}</p>
-                                <p className="username">@{user?.username}</p>
+                                <p className="display_name">{displayName}</p>
+                                <p className="username">@{username}</p>
                             </div>
 
                             <IconContext.Provider value={{ className: "navbar-account_icon" }}>
