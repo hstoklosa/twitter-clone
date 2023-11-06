10993ad4ea749702a4dfa1f7cbe6a0f27d143de0
imexotic <hubert.stoklosa23@gmail.com>
2023-11-06 15:35:04 +0000

    integrate react-router into TabList component for routing capabilities

--- client/src/components/shared/TabList.jsx
+++ client/src/components/shared/TabList.jsx
@@ -1,17 +1,36 @@
 import "../../styles/TabList.css";
 
-const TabList = ({ tabs = [], currentTab, setCurrentTab, children }) => {
+import { Link, useLocation } from "react-router-dom";
+import { capitalize } from "../../helpers/capitalize";
+
+// const getPreviousPathname = (pathname) => pathname.split("/").slice(0, -1).join("/");
+
+const TabList = ({ tabs = [], setTab, options = {} }) => {
+    const { state, pathname } = useLocation();
+
     return (
         <div className="tab-list">
-            {tabs.map((tab, idx) => (
-                <button
-                    key={idx}
-                    className={`tab-item ${currentTab === tab && "active"}`}
-                    onClick={() => setCurrentTab(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
+            {tabs.map((tab, idx) => {
+                const isTabIndex = tab === options.indexTab;
+                const link = isTabIndex ? options.index : `${options.index}/${tab}`;
+                const isActive = pathname.endsWith(`${isTabIndex ? options.index : tab}`);
+
+                const linkOptions = {
+                    to: link,
+                    state: state,
+                    onClick: () => setTab(tab),
+                };
+
+                return (
+                    <Link
+                        {...linkOptions}
+                        className={`tab-item ${isActive && "active"}`}
+                        key={idx}
+                    >
+                        {capitalize(tab)}
+                    </Link>
+                );
+            })}
         </div>
     );
 };
