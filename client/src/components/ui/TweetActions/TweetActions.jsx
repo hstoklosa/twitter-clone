95a1bc5f315d78aa10103c04e5966179e33522bc
imexotic <hubert.stoklosa23@gmail.com>
2023-12-17 13:58:22 +0000

    update tweet actions

--- client/src/components/ui/TweetActions/TweetActions.jsx
+++ client/src/components/ui/TweetActions/TweetActions.jsx
@@ -1,135 +1,191 @@
-import "react-circular-progressbar/dist/styles.css";
 import "./styles.css";
 
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-
+import { useState } from "react";
 import { IconContext } from "react-icons";
-import { HiOutlineLocationMarker } from "react-icons/hi";
-import { TbCalendarTime, TbMoodSmile } from "react-icons/tb";
-import { MdOutlinePoll, MdOutlineGifBox } from "react-icons/md";
-import { PiImageSquareBold } from "react-icons/pi";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { PiPencilSimpleLine } from "react-icons/pi";
+import { LuRepeat2 } from "react-icons/lu";
+import { TbMessageCircle2, TbShare2 } from "react-icons/tb";
+import { IoMdStats } from "react-icons/io";
+import { BiBookmark, BiSolidBookmark } from "react-icons/bi";
 
-const TweetActions = ({ maxLength, tweet, handleTweet, setMedia, setMediaPreview }) => {
-    const handleFileChange = ({ target }) => {
-        const types = ["image/png", "image/jpeg", "image/jpg"];
-        const files = target.files;
-        const image = files?.[0];
+import { LinkButton, FloatOptions } from "../../index"
 
-        if (image && types.includes(image.type)) {
-            const blob = URL.createObjectURL(image);
+import {
+    useLikeTweetMutation,
+    useUnlikeTweetMutation,
+    useCreateRepostMutation,
+    useDeleteRepostMutation,
+    useCreateBookmarkMutation,
+    useDeleteBookmarkMutation,
+} from "../../../features/api/userApi";
 
-            setMedia(image);
-            setMediaPreview(blob);
-        }
+const TweetActions = ({ tweet, currentUser, openReplyModal, openQuoteModal }) => {
+    const [retweetFloat, setRetweetFloat] = useState(false);
+
+    const [createRepost] = useCreateRepostMutation();
+    const [deleteRepost] = useDeleteRepostMutation();
+    const [likeTweet] = useLikeTweetMutation();
+    const [unlikeTweet] = useUnlikeTweetMutation();
+    const [createBookmark] = useCreateBookmarkMutation();
+    const [deleteBookmark] = useDeleteBookmarkMutation();
+
+    const isBookmarked = currentUser.bookmarks.includes(tweet._id);
+    const isReposted = tweet.retweets.includes(currentUser._id);
+    const isLiked = tweet.likes.includes(currentUser._id);
+
+    const handleRetweet = async (e) => {
+        const retweetResult = isReposted
+            ? await deleteRepost({ tweetId: tweet._id })
+            : await createRepost({ tweetId: tweet._id });
+
+        if (retweetResult?.error) return;
+
+        if (
+            (isReposted && !retweetResult.data?.isReposted) ||
+            (!isReposted && retweetResult.data?.isReposted)
+        )
+            closeRetweetFloat();
     };
+
+    const handleLike = async (e) => {
+        isLiked
+            ? await unlikeTweet({ id: tweet._id })
+            : await likeTweet({ id: tweet._id });
+    };
+
+    const handleBookmark = async () => {
+        const bookmarkData = {
+            tweetId: tweet._id,
+            userId: currentUser.id,
+        };
+
+        isBookmarked
+            ? await deleteBookmark(bookmarkData)
+            : await createBookmark(bookmarkData);
+    };
+
+
+    const openRetweetFloat = () => setRetweetFloat(true);
+    const closeRetweetFloat = () => setRetweetFloat(false);
 
     return (
         <div className="tweet-actions">
-            <div className="attachments">
-                <IconContext.Provider value={{ className: "icon tweet-attachments_icon" }}>
-                    <label
-                        type="button"
-                        className="tweet-attachments_button blue_round-btn"
-                    >
-                        <input
-                            type="file"
-                            id="image"
-                            accept=".jpg, .jpeg, .png"
-                            style={{
-                                display: "none",
-                            }}
-                            onChange={handleFileChange}
-                        />
-                        <div className="icon-container">
-                            <PiImageSquareBold size="16" />
-                        </div>
-                    </label>
+            <LinkButton
+                className={`action-btn reply blue_round-btn`}
+                onClick={openReplyModal}
+            >
+                <div className="icon-container">
+                    <TbMessageCircle2 className="icon" />
+                </div>
 
-                    <button
-                        type="button"
-                        className="tweet-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <MdOutlineGifBox size="16" />
-                        </div>
-                    </button>
+                <div className="count-container">
+                    <span>{tweet.repliesCount}</span>
+                </div>
+            </LinkButton>
 
-                    <button
-                        type="button"
-                        className="tweet-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <MdOutlinePoll size="16" />
-                        </div>
-                    </button>
+            <LinkButton
+                className={`action-btn retweet green_round-btn ${isReposted && "applied"}`}
+                onClick={openRetweetFloat}
+            >
+                <div className="icon-container">
+                    <LuRepeat2 className="icon" />
+                </div>
 
-                    <button
-                        type="button"
-                        className="tweet-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <TbMoodSmile size="16" />
-                        </div>
-                    </button>
+                <div className="count-container">
+                    <span>{tweet.retweets.length}</span>
+                </div>
 
-                    <button
-                        type="button"
-                        className="tweet-attachments_button blue_round-btn"
-                        disabled
+                {retweetFloat && (
+                    <IconContext.Provider
+                        value={{ className: "float-icon" }}
                     >
-                        <div className="icon-container">
-                            <TbCalendarTime size="16" />
-                        </div>
-                    </button>
+                        <FloatOptions
+                            isOpen={retweetFloat}
+                            onClose={closeRetweetFloat}
+                            className="retweet-options"
+                        >
+                            <LinkButton
+                                type="button"
+                                className="float-btn"
+                                onClick={handleRetweet}
+                            >
+                                <div className="float-icon-container">
+                                    <LuRepeat2 />
+                                </div>
 
-                    <button
-                        type="button"
-                        className="tweet-attachments_button blue_round-btn"
-                        disabled
-                    >
-                        <div className="icon-container">
-                            <HiOutlineLocationMarker size="16" />
-                        </div>
-                    </button>
-                </IconContext.Provider>
-            </div>
+                                {isReposted ? "Undo Repost" : "Repost"}
+                            </LinkButton>
 
-            <div className="tweet-submit">
-                {tweet.length > 0 && (
-                    <div
-                        className="progressbar-container"
-                        style={{
-                            width: 25,
-                            height: 25,
-                        }}
-                    >
-                        <CircularProgressbar
-                            value={(tweet.length / 280) * 100}
-                            styles={buildStyles({
-                                pathColor: tweet.length === maxLength ? "red" : "#1D9BF0",
-                                trailColor: "#2F3336",
-                                backgroundColor: "#3e98c7",
-                            })}
-                        />
-                    </div>
+                            <LinkButton
+                                type="button"
+                                className="float-btn"
+                                onClick={openQuoteModal}
+                            >
+                                <div className="float-icon-container">
+                                    <PiPencilSimpleLine size="21" />
+                                </div>
+                                Quote
+                            </LinkButton>
+                        </FloatOptions>
+                    </IconContext.Provider>
                 )}
+            </LinkButton>
 
-                {tweet.length > 0 && <div className="separator"></div>}
+            <LinkButton
+                type="button"
+                className={`action-btn like red_round-btn ${isLiked && "applied"}`}
+                data-type="inner-button"
+                onClick={handleLike}
+            >
+                <div className="icon-container">
+                    {isLiked ? <AiFillHeart className="icon" /> : <AiOutlineHeart className="icon" />}
+                </div>
 
-                <button
+                <div className="count-container">
+                    <span>{tweet.likes.length}</span>
+                </div>
+            </LinkButton>
+
+            <LinkButton
+                className="action-btn view blue_round-btn"
+                disabled
+            >
+                <div className="icon-container">
+                    <IoMdStats className="icon" />
+                </div>
+
+                <div className="count-container">
+                    <span>0</span>
+                </div>
+            </LinkButton>
+
+            <div className="tweet-actions-container">
+                <LinkButton
                     type="button"
-                    className="blue-btn post-btn"
-                    disabled={tweet.length <= 0}
-                    onClick={handleTweet}
+                    className="action-btn bookmark blue_round-btn"
+                    onClick={handleBookmark}
                 >
-                    Tweet
-                </button>
+                    <div className="icon-container">
+                        {isBookmarked ? (
+                            <BiSolidBookmark className="icon" />
+                        ) : (
+                            <BiBookmark className="icon" />
+                        )}
+                    </div>
+                </LinkButton>
+
+                <LinkButton
+                    className="action-btn tweet-btn blue_round-btn"
+                    disabled
+                >
+                    <div className="icon-container">
+                        <TbShare2 className="icon" />
+                    </div>
+                </LinkButton>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default TweetActions;
