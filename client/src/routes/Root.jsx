0cc8fea1c1fe8109efd3acc85d15a4efee76a714
imexotic <hubert.stoklosa23@gmail.com>
2023-12-21 20:51:34 +0000

    integrate react-hot-toast

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,6 +1,7 @@
 import "../routes/Login/styles.css";
 
 import { Outlet } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 
 import { useAppSelector } from "../app/store";
 import { useCheckAuthQuery } from "../features/api/authApi";
@@ -9,6 +10,16 @@ import { useGetUserInfoQuery } from "../features/api/userApi";
 import { useTheme } from "../contexts/ThemeProvider";
 
 import { Loading } from "../components";
+
+const toastOptions = {
+    style: {
+        color: 'white',
+        borderRadius: '4px',
+        backgroundColor: 'var(--primary-colour)',
+        zIndex: "999999999"
+    },
+    success: { duration: 4000 }
+};
 
 const Root = () => {
     const auth = useAppSelector((state) => state.auth);
@@ -23,9 +34,14 @@ const Root = () => {
 
     return (
         <div id="app" data-theme={theme} data-accent={accent}>
+            <Toaster
+                position="bottom-center"
+                toastOptions={toastOptions}
+                reverseOrder={false}
+            />
+
             <div id="app-container">
                 {(isAuthLoading || isUserLoading) ? <Loading /> : <Outlet />}
-
             </div>
         </div>
     );
