4893a8913db55ed98942830e100773bbbf68e7d9
hstoklosa <hubert.stoklosa23@gmail.com>
2024-05-09 23:24:37 +0100

    modify MedialModal to use global modals

--- client/src/components/modals/MediaModal/MediaModal.jsx
+++ client/src/components/modals/MediaModal/MediaModal.jsx
@@ -1,36 +1,59 @@
 import "./styles.css";
 
+import { useRef, useEffect } from "react";
+import { useMediaQuery } from "@uidotdev/usehooks";
 import { IoMdClose } from "react-icons/io";
 import { BaseModal } from "../../index";
 
-const MediaModal = ({ isOpen, closeMediaModal, mediaUrl }) => {
+import { useAppSelector, useAppDispatch } from "../../../app/store";
+import { modalActions } from "../../../features/slices/modalSlice";
+
+const MediaModal = ({ url }) => {
+    const { isOpen } = useAppSelector((state) => state.modal);
+    const dispatch = useAppDispatch();
+
+    const ref = useRef(null);
+
+    const isMediumDevice = useMediaQuery("only screen and (max-width : 620px)");
+
+    useEffect(() => {
+        const element = ref.current;
+
+        if (element) {
+            const parent = element.parentNode;
+
+            if (element.offsetWidth > element.offsetHeight) {
+                parent.style.width = "100%";
+            } else {
+                if (!isMediumDevice) {
+                    // parent.style.width = "500px";
+                    parent.style.width = "auto";
+                    parent.style.height = "100%"
+                } else {
+                    parent.style.width = "100%";
+                    parent.style.height = "auto"
+                }
+            }
+        }
+
+    }, [ref, isMediumDevice])
+
     return (
         <BaseModal
             isOpen={isOpen}
-            onClose={closeMediaModal}
+            onClose={() => dispatch(modalActions.closeModal())}
             className="media-modal"
         >
-            <div className="media-modal_content">
-                <img
-                    src={mediaUrl}
-                    className="media-modal_media"
-                    alt="Media"
-                />
-
-
-                <a
-                    className="media-modal_link"
-                    href={mediaUrl}
-                    target="__blank"
-                >
-                    Open original
-                </a>
-
-            </div>
+            <img
+                src={url}
+                className="media-modal_media"
+                alt="Media"
+                ref={ref}
+            />
 
             <button
                 className="media-modal_close dark_round-btn"
-                onClick={closeMediaModal}
+                onClick={() => dispatch(modalActions.closeModal())}
             >
                 <div className="icon-container">
                     <IoMdClose
