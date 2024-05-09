fcfe0c33dba9171c9f4fe3ba99f8e720e503d2c3
hstoklosa <hubert.stoklosa23@gmail.com>
2024-05-09 23:49:03 +0100

    add ActionModal to unfollowing users

--- client/src/components/common/FollowButton.jsx
+++ client/src/components/common/FollowButton.jsx
@@ -1,26 +1,41 @@
-import { useAppSelector } from "../../app/store";
+import { useAppSelector, useAppDispatch } from "../../app/store";
+import { modalActions } from "../../features/slices/modalSlice";
 import {
     useFollowUserMutation,
     useUnfollowUserMutation,
 } from "../../features/api/userApi";
-import { LinkButton } from "./index";
 
-const FollowButton = ({ targetUserId, isFollowing }) => {
+import { LinkButton } from "../index";
+
+
+const _desc = "Their posts will no longer show up in your For You timeline. You can still view their profile, unless their posts are protected.";
+
+const FollowButton = ({ targetUserName, targetUserId, isFollowing }) => {
     const { user: currentUser } = useAppSelector((state) => state.auth);
+    const dispatch = useAppDispatch();
 
     const [followUser] = useFollowUserMutation();
     const [unfollowUser] = useUnfollowUserMutation();
 
-    const handleFollow = async () => {
-        const followData = {
-            id: currentUser.id,
-            targetUserId: targetUserId,
-        };
+    const handleUnfollowAction = async (e) => {
+        await unfollowUser({ id: currentUser.id, targetUserId: targetUserId });
+    }
 
-        return isFollowing
-            ? await unfollowUser(followData)
-            : await followUser(followData);
+    const handleFollow = async (e) => {
+        !isFollowing
+            ? await followUser({ id: currentUser.id, targetUserId: targetUserId })
+            : dispatch(modalActions.openModal({
+                name: "ActionModal",
+                props: {
+                    title: `Unfollow the @${targetUserName}?`,
+                    description: _desc,
+                    mainBtnLabel: "Unfollow",
+                    focusOnMainBtn: true,
+                    action: handleUnfollowAction,
+                }
+            }))
     };
+
 
     return (
         <LinkButton
